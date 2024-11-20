eb77945cfe2f2b459b9ded57fc0ce23bc542552d
Haikotjo <haikowierda@hotmail.com>
2024-11-20 11:50:00 +0100

    Add font added theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import theme from "./themes/theme";
 import "./index.css";
 import './styles/cssReset.css';
+import {ThemeProvider} from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
