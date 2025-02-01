fc79701e3c22513e8c872a2749e4c17fc382a4d4
Haikotjo <haikowierda@hotmail.com>
2025-02-01 11:05:34 +0100

    Added icons to nutrient list

--- src/components/mealCard/nutrientList/NutrientList.jsx
+++ src/components/mealCard/nutrientList/NutrientList.jsx
@@ -1,9 +1,17 @@
 import PropTypes from "prop-types";
 import { Box } from "@mui/material";
+import { Flame, Dumbbell, Droplet, ChartColumnIncreasing } from "lucide-react";
 
 const NutrientList = ({ nutrients }) => {
+    const nutrientIcons = {
+        "Energy kcal": <Flame size={16} style={{ marginRight: "5px" }} />,
+        "Protein g": <Dumbbell size={16} style={{ marginRight: "5px" }} />,
+        "Total lipid (fat) g": <Droplet size={16} style={{ marginRight: "5px" }} />,
+        "Carbohydrates g": <ChartColumnIncreasing size={16} style={{ marginRight: "5px" }} />,
+    };
+
     const filteredNutrients = nutrients.filter((nutrient) =>
-        ["Energy kcal", "Total lipid (fat) g", "Carbohydrates g", "Protein g"].includes(nutrient.nutrientName)
+        Object.keys(nutrientIcons).includes(nutrient.nutrientName)
     );
 
     return (
@@ -13,14 +21,19 @@ const NutrientList = ({ nutrients }) => {
                     key={nutrient.nutrientId}
                     component="li"
                     sx={{
+                        display: "flex",
+                        alignItems: "center",
                         fontSize: {
                             xs: "0.7rem",
                             sm: "0.8rem",
                         },
+                        marginBottom: "5px",
                         fontFamily: "'Quicksand', sans-serif",
                     }}
                 >
+                    {nutrientIcons[nutrient.nutrientName]}
                     {nutrient.nutrientName}: {nutrient.value ? nutrient.value.toFixed(1) : "N/A"} {nutrient.unitName}
+
                 </Box>
             ))}
         </ul>
@@ -30,7 +43,7 @@ const NutrientList = ({ nutrients }) => {
 NutrientList.propTypes = {
     nutrients: PropTypes.arrayOf(
         PropTypes.shape({
-            nutrientId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired, // Accepteer zowel string als number
+            nutrientId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
             nutrientName: PropTypes.string.isRequired,
             value: PropTypes.number,
             unitName: PropTypes.string.isRequired,
