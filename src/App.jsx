2680432c4853caf1d4c53e6ac47f3f06a9f2f762
Haikotjo <haikowierda@hotmail.com>
2025-08-01 21:22:18 +0200

    Fix for icons in submenu v5.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import AppRoutes from "./routes/Routes.jsx";
+import { BookOpen, Soup, UserPen } from "lucide-react";
 
 function App() {
     console.log("🚀 VITE_BASE_URL =", import.meta.env.VITE_BASE_URL);
     return (
-        <AppRoutes/>
-    );
+        <>
+        <div style={{display: "none"}}>
+            <BookOpen/>
+            <Soup/>
+            <UserPen/>
+        </div>
+
+    <AppRoutes/>
+</>
+)
+    ;
 }
+
 export default App;
