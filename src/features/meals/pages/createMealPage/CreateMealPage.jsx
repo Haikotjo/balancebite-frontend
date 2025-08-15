51795ffab32c09925f8600fa8baf6715c3995329
Haikotjo <haikowierda@hotmail.com>
2025-08-15 21:19:11 +0200

    Fixing the new size of appbar so pages will fit. Adding pagewrapper to more pages. CreateMeal wrapper and added a soup icon v1.4

--- src/features/meals/pages/createMealPage/CreateMealPage.jsx
+++ src/features/meals/pages/createMealPage/CreateMealPage.jsx
@@ -1,11 +1,22 @@
+// CreateMealPage.jsx
+import { Soup } from "lucide-react";                    // Icon
+import PageWrapper from "../../../../components/layout/PageWrapper.jsx";
 import CreateMealForm from "../../components/createMealForm/CreateMealForm.jsx";
 import CustomBox from "../../../../components/layout/CustomBox.jsx";
 
 const CreateMealPage = () => {
     return (
-        <CustomBox className="max-w-screen-md mx-auto pt-10 p-4">
-            <CreateMealForm />
-        </CustomBox>
+        // Page shell: handles sidebar/bottombar offsets + base paddings
+        <PageWrapper className="max-w-screen-md mx-auto">
+            {/* Center content + large icon above the form */}
+            <CustomBox className="flex flex-col items-center">
+                {/* Big page icon */}
+                <Soup className="w-16 h-16 sm:w-24 sm:h-24 text-primary mb-4" aria-hidden="true" />
+
+                {/* Form */}
+                <CreateMealForm />
+            </CustomBox>
+        </PageWrapper>
     );
 };
 
