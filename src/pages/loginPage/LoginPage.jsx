8f6d8c985a4a938616187ce63354dab15ba139ab
Haikotjo <haikowierda@hotmail.com>
2025-08-16 21:51:20 +0200

    Fixing the new pages with pagewrapper. Login and Register pages v1

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,15 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import CustomBox from "../../components/layout/CustomBox.jsx";
 import LoginForm from "../../features/navigation/components/authLoginForm/LoginForm.jsx";
-
+import PageWrapper from "../../components/layout/PageWrapper.jsx";
 
 function LoginPage() {
     const navigate = useNavigate();
 
     return (
-        <CustomBox className="flex flex-col items-center min-h-screen p-2">
-            <LoginForm onSwitchToRegister={() => navigate("/register")} />
-        </CustomBox>
+        <PageWrapper narrow>
+            <CustomBox className="flex flex-col items-center">
+                <LoginForm onSwitchToRegister={() => navigate("/register")} />
+            </CustomBox>
+        </PageWrapper>
     );
 }
 
--- src/pages/registerPage/RegisterPage.jsx
+++ src/pages/registerPage/RegisterPage.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import CustomBox from "../../components/layout/CustomBox.jsx";
 import RegisterForm from "../../features/navigation/components/authRegisterForm/RegisterForm.jsx";
+import PageWrapper from "../../components/layout/PageWrapper.jsx";
 
 function RegisterPage() {
     const navigate = useNavigate();
 
     return (
-        <CustomBox className="flex flex-col items-center min-h-screen p-2 pt-4">
-            <RegisterForm onSwitchToLogin={() => navigate("/login")} />
-        </CustomBox>
+        <PageWrapper narrow>
+            <CustomBox className="flex flex-col items-center">
+                <RegisterForm onSwitchToLogin={() => navigate("/login")} />
+            </CustomBox>
+        </PageWrapper>
     );
 }
 
