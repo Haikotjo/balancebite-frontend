6f1af1a1bfbbc215a19b33922f71ed54387f245b
Haikotjo <haikowierda@hotmail.com>
2025-08-16 22:50:50 +0200

    Fixing the new pages with pagewrapper. UpdatemealPage v1

--- src/features/meals/pages/updateMealPage/UpdateMealPage.jsx
+++ src/features/meals/pages/updateMealPage/UpdateMealPage.jsx
@@ -1,11 +1,15 @@
 import CustomBox from "../../../../components/layout/CustomBox.jsx";
 import UpdateMealForm from "../../components/updateMealForm/UpdateMealForm.jsx";
+import PageWrapper from "../../../../components/layout/PageWrapper.jsx";
 
 const UpdateMealPage = () => {
     return (
-        <CustomBox className="max-w-[600px] mx-auto px-2 sm:px-4 pb-[4rem] sm:pb-[2rem] overflow-y-auto h-[calc(100vh-64px)] md:overflow-visible md:h-auto">
-            <UpdateMealForm />
-        </CustomBox>
+        <PageWrapper>
+            {/* Constrain form width and center */}
+            <CustomBox className="max-w-screen-md mx-auto">
+                <UpdateMealForm />
+            </CustomBox>
+        </PageWrapper>
     );
 };
 
