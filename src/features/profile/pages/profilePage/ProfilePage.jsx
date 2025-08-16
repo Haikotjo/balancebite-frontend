bb309a4e695ab79bdfefb6a135e7f774bf9fc61f
Haikotjo <haikowierda@hotmail.com>
2025-08-16 22:59:14 +0200

    Fixing the new pages with pagewrapper. ProfilePage. Adding title and icon v1.1

--- src/features/profile/pages/profilePage/ProfilePage.jsx
+++ src/features/profile/pages/profilePage/ProfilePage.jsx
@@ -5,6 +5,7 @@ import CustomTypography from "../../../../components/layout/CustomTypography.jsx
 import PersonalInfoForm from "../../components/personalInfoForm/PersonalInfoForm.jsx";
 import CustomCard from "../../../../components/layout/CustomCard.jsx";
 import PageWrapper from "../../../../components/layout/PageWrapper.jsx";
+import {UserCircle} from "lucide-react";
 
 /**
  * ProfilePage component
@@ -37,6 +38,13 @@ const ProfilePage = () => {
         <PageWrapper>
             {/* Center content and constrain width */}
             <CustomBox className="max-w-screen-xl mx-auto flex flex-col gap-5">
+                {/* Form header: icon + title */}
+                <CustomBox className="flex flex-col items-center gap-3 mb-2">
+                    <UserCircle className="w-16 h-16 sm:w-24 sm:h-24 text-primary" aria-hidden="true" />
+                    <CustomTypography as="h2" variant="h1" className="text-center">
+                        Create Your Meal
+                    </CustomTypography>
+                </CustomBox>
                 {/* Personal Info & User Details Section */}
                 <CustomBox className="grid grid-cols-1 md:grid-cols-2 gap-4 items-start">
                     {/* Personal Information Form */}
